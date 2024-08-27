a1fe579689835be30cb3a58c5c2880497c6fd544
altantheprodigy <altanpoke9@gmail.com>
2024-08-27 23:22:15 +0700

    feat [AP] Feat: fetch api

--- src/Page/TransactionHistoryPage/DummyTableData/DummyTable.jsx
+++ src/Page/TransactionHistoryPage/DummyTableData/DummyTable.jsx
@@ -1,28 +1,29 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrash } from '@fortawesome/free-solid-svg-icons';
+import {getTransaksiList} from "../../../Api/ApiTransaksi.jsx";
 
 function TableComponent() {
-    const generateData = () => {
-        let generatedData = [];
-        for (let i = 1; i <= 50; i++) {
-            generatedData.push({
-                id: i,
-                name: `Produk ${i}`,
-                mitra: `Mitra ${i}`,
-                pembayaran: `Dana ${i}`,
-                price: `$ ${i}`,
-                category: "Produk",
-                action: <FontAwesomeIcon icon={faTrash} />,
-            });
+    const [transaksi, setTransaksi] = useState([]);
+
+    useEffect(() => {
+        fetchTransaksi();
+    }, []);
+
+    const fetchTransaksi = async () => {
+        try {
+            const result = await getTransaksiList();
+            if (result && Array.isArray(result.cods)) {
+                setTransaksi(result.cods);
+                console.log(result.cods);
+            } else {
+                console.error("Invalid Transaksi data structure:", result);
+            }
+        } catch (e) {
+            console.error("Eror Fetching data transaksi", e)
         }
-        return generatedData;
-    };
-
-    const data = generateData();
-
-
+    }
     return (
         <div className="table-auto overflow-auto h-[535px]">
             <table className="min-w-full bg-white border-collapse border border-gray-300 rounded-lg shadow-custom-light">
@@ -32,20 +33,20 @@ function TableComponent() {
                     <th className="table-header">Nama Produk</th>
                     <th className="table-header">Mitra</th>
                     <th className="table-header">Harga</th>
-                    <th className="table-header">Category</th>
+                    <th className="table-header">Pemesan</th>
                     <th className="table-header">Metode Pembayaran</th>
                     <th className="table-header">Action</th>
                 </tr>
                 </thead>
                 <tbody>
-                {data.map((item) => (
+                {transaksi.map((item) => (
                     <tr key={item.id} className="hover:bg-gray-100">
                         <td className="py-2 px-4 border border-gray-300 text-center">{item.id}</td>
-                        <td className="table-down">{item.name}</td>
-                        <td className="table-down">{item.mitra}</td>
-                        <td className="table-down">{item.price}</td>
-                        <td className="table-down">{item.category}</td>
-                        <td className="table-down">{item.pembayaran}</td>
+                        <td className="table-down">{item.barang.nama_barang}</td>
+                        <td className="table-down">{item.mitra_id}</td>
+                        <td className="table-down">{item.grand_total}</td>
+                        <td className="table-down">{item.user.name}</td>
+                        <td className="table-down">{item.status_pembayaran}</td>
                         <td className="table-down text-[#FD0404]">{item.action}</td>
                     </tr>
                 ))}
